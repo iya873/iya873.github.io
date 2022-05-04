fa190457ac9eb7907c15b8080a48e4cf4a22299a
Iya <siyana873@gmail.com>
2022-05-04 17:04:10 -0400

    got game to appear when button clicked but sizing is off

---
+++ MusicTrivia/app.js
@@ -0,0 +1,33 @@
+// let score = 0;
+
+// document.querySelector(".score").innerText = score;
+
+// function increase() {
+//   score = score + 1;
+//   document.getElementsByClassName(".score").innerText = score;
+// }
+
+// function decrease() {
+//   score = score - 1;
+//   document.getElementsByClassName(".score").innerText = score;
+// }
+
+// let updateScore = () => {
+//   let correctAnswer = true;
+//   if (correctAnswer = true; score + cardValue);
+
+//   cardValue = () => {
+//     let value =
+//   }
+// }
+
+// make button show game on click
+
+let showGame = function() {
+    // get the button
+    var button = document.getElementById("start");
+    // get the gameboard
+    var game = document.getElementById("game");
+    game.style.display = "block";
+  }
+  button.addEventListener("click", showGame);
