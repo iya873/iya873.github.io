73e4c4476ee676316f3294663699417e1ece56d3
Iya <siyana873@gmail.com>
2022-05-05 23:36:36 -0400

    still refactoring basic functions of game playing around with different methods and function

--- MusicTrivia/app.js
+++ MusicTrivia/app.js
@@ -1,6 +1,6 @@
 // let score = 0;
 
-// document.querySelector(".score").innerText = score;
+// // document.querySelector(".score").innerText = score;
 
 // function increase() {
 //   score = score + 1;
@@ -23,11 +23,70 @@
 
 // make button show game on click
 
-let showGame = function() {
-    // get the button
-    var button = document.getElementById("start");
-    // get the gameboard
-    var game = document.getElementById("game");
-    game.style.display = "block";
-  }
+let showGame = () => {
+  // get the button
+  var button = document.getElementById("start");
+  // get the gameboard
+  var game = document.getElementById("game");
+  game.style.display = "block";
   button.addEventListener("click", showGame);
+
+}
+var gameStarted = true;
+// var user1 = 1;
+let currentPlayer = document.getElementById('score1');
+
+if (gameStarted === true) {
+  currentPlayer.style.background = 'green';
+};
+
+
+  var qTwos = {
+    question: "Souled Out is the name of whose album?",
+    points: 200,
+    answer: "Jhene Aiko"
+
+};
+
+document.querySelector('.cards').addEventListener('click', showQuestion)
+
+function showQuestion(e) {
+  // console.log(e.target.tagName);
+  if (e.target.tagName === 'TD') {
+    let guess = prompt(qTwos.question);
+    let show = document.getElementsByTagName('td');
+    let catgeories = document.getElementsByTagName('thead');
+    checkAnswer(guess);
+}
+}
+// show.addEventListener('click', message)
+function checkAnswer(guess) {
+  let answeredGuess = true;
+  if (guess === qTwos.answer.toLowerCase()) {
+    alert('You got it right!');
+    updateScore(true);
+  } else {
+    alert('You got it wrong!');
+    updateScore(false);
+  }
+}
+
+var scores = document.getElementsByClassName('scores');
+
+scores.addEventListener('click', 'updateScore');
+
+function updateScore(isCorrect) {
+  if (isCorrect === true) {
+    scores.target.innerHTML = parseInt(scores[0].innerHTML) + qTwos.points
+  } else {
+    scores.target.innerHTML = parseInt(scores[0].innerHtml) - qTwos.points
+  }
+}
+let cardAnswered = true
+
+function nextTurn() {
+  if (cardAnswered === true) {
+    let nextPlayer = document.getElementById('score2').style.background = 'green';
+    currentPlayer.style.background = 'yellow';
+  }
+}
---
+++ MusicTrivia/questions.js
@@ -0,0 +1,29 @@
+// const qTwos = {
+//     question: "Souled Out is the name of whose album?",
+//     points: 200,
+//     answer: "Jhene Aiko"
+
+// }
+
+// var showQuestion = () => {
+//     prompt('qTwos.question');
+// }
+// const card = document.querySelector('tbody');
+// tbodyElement.addEventListener('click', showQuestion)
+// var currentPlayer = true;
+// new function() {
+//     if (currentPlayer === true) {
+//         let glow = document.getElementById('playerOne');
+//         glow.style.color = 'yellow';
+//     }
+// }
+// class QuestionData {
+//     constructor(question, answer, points, genre) {
+//         this.question = question;
+//         this.answer = answer;
+//         this.points = points,
+//         this.genre = genre;
+//     }
+// }
+// let qTwo1 = new QuestionData('Souled Out is the name of whose debut album?', 'Jhene Aiko', 200, 'West Coast');
+// let qTwo2 = new QuestionData('This sekf-titled album had its own "superpower', 'BEYONCE', 200, 'Duurty South');
